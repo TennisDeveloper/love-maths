6ac1425b3c3c5d118405e00e3187e52a44b69d95
Barbora Fabianova <BARBORKA.FABIANOVA@GMAIL.COM>
2023-02-04 20:25:33 +0000

    Final comit with division game included.

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,9 @@ function runGame(gameType) {
         } else if (gameType === "subtract") {
           displaySubstractQuestion (num1, num2);  
 
+        } else if (gameType === "division") {
+        displayDivisionQuestion (num1, num2);
+
         } else {
         alert(`Unknown game tpye: ${gameType}`);
         throw `Unknown game type: ${gameType}. Abborting!`;
@@ -83,7 +86,11 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } {
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+
+    } else
+    {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Abborting!`;
     }
@@ -132,3 +139,10 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent= operand1 * operand2;
+    document.getElementById('operand2').textContent= operand2;
+    document.getElementById('operator').textContent= "/";
+
+}
